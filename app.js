73fe0fbeca6f5f0817e83a416b38eb8bebfbeab0
Gustavo Muller <gusrmuller@gmail.com>
2023-04-03 05:14:02 -0300

    change icon based on weather

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inputPart = document.querySelector('.input-part');
 const infoTxt = document.querySelector('.info-txt');
 const inputField = document.querySelector('input');
 const locationBtn = document.querySelector('button');
+const wIcon = document.querySelector('.weather-part img');
 
 let api;
 
@@ -13,7 +14,7 @@ inputField.addEventListener('keyup', e => {
 });
 
 locationBtn.addEventListener('click', () => {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
     } else {
         alert('Seu navegador não suporta a geolocalização.')
@@ -21,7 +22,7 @@ locationBtn.addEventListener('click', () => {
 })
 
 function onSuccess(position) {
-    const {latitude, longitude} = position.coords;
+    const { latitude, longitude } = position.coords;
     api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=5b34f67a5a92dd5e00d4c0c1a36452a4`;
     fetchData();
 }
@@ -40,26 +41,40 @@ function fetchData() {
     infoTxt.innerText = 'Obtendo informações do clima...';
     infoTxt.classList.add('pending');
     fetch(api)
-    .then(response => response.json())
-    .then(result => weatherDetails(result));
+        .then(response => response.json())
+        .then(result => weatherDetails(result));
 }
 
 function weatherDetails(info) {
-    if(info.cod == '404') {
+    if (info.cod == '404') {
         infoTxt.innerText = `${inputField.value} não é uma cidade válida`
         infoTxt.classList.replace('pending', 'error')
     } else {
         const city = info.name;
         const country = info.sys.country;
-        const {description, id} = info.weather[0];
-        const {feels_like, humidity, temp} = info.main;
+        const { description, id } = info.weather[0];
+        const { feels_like, humidity, temp } = info.main;
+
+        if (id == 800) {
+            wIcon.src = 'icons/clear.svg';
+        } else if (id >= 200 && id <= 232) {
+            wIcon.src = 'icons/storm.svg';
+        } else if (id >= 600 && id <= 622) {
+            wIcon.src = 'icons/snow.svg';
+        } else if (id >= 701 && id <= 781) {
+            wIcon.src = 'icons/haze.svg';
+        } else if (id >= 801 && id <= 804) {
+            wIcon.src = 'icons/cloud.svg';
+        } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+            wIcon.src = 'icons/rain.svg';
+        }
 
         wrapper.querySelector('.temp .numb').innerText = Math.floor(temp);
         wrapper.querySelector('.weather').innerText = description;
         wrapper.querySelector('.location span').innerText = `${city}, ${country}`;
         wrapper.querySelector('.temp .numb-2').innerText = Math.floor(feels_like);
         wrapper.querySelector('.humidity span').innerText = `${humidity}%`;
-        
+
         infoTxt.classList.remove('pending', 'error');
         wrapper.classList.add('active');
         console.log(info);
